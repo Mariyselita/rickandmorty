f4a80373d349043f144fb0d8d01695d159d3b5e9
marisela Cadena <mariyselita@MacBook-Pro-de-marisela.local>
2020-01-13 00:58:24 -0600

    Updates

---
+++ precache-manifest.50adc08d7200c47f99f56403264c2829.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d3ae284e7eb31bef6308f0564a3e67",
+    "url": "/ryckandmorty/index.html"
+  },
+  {
+    "revision": "6e9d08bd3f13ba632e86",
+    "url": "/ryckandmorty/static/css/2.48d2f880.chunk.css"
+  },
+  {
+    "revision": "e5774686110cb7bc0354",
+    "url": "/ryckandmorty/static/css/main.f615eeff.chunk.css"
+  },
+  {
+    "revision": "6e9d08bd3f13ba632e86",
+    "url": "/ryckandmorty/static/js/2.44700998.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/ryckandmorty/static/js/2.44700998.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5774686110cb7bc0354",
+    "url": "/ryckandmorty/static/js/main.fd0a7392.chunk.js"
+  },
+  {
+    "revision": "27e11f4b50b04819c5a4",
+    "url": "/ryckandmorty/static/js/runtime-main.221ebd89.js"
+  },
+  {
+    "revision": "1b16577cdae7ccef2acad41e3fb94fe7",
+    "url": "/ryckandmorty/static/media/ep-3.1b16577c.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ryckandmorty/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ryckandmorty/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ryckandmorty/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ryckandmorty/static/media/slick.f97e3bbf.svg"
+  }
+]);
